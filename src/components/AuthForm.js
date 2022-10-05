7bd238522eb0adb139932ff96d4b1f1a7dc235d7
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-10-05 14:57:47 +0530

    Update AuthForm.js

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -451,7 +451,7 @@ sendOTP=()=>{
                       </a>
                       <br />
                       <br />
-                      <a href="https://t.me/colortrading247">
+                      <a href="https://t.me/terion101">
                         Contact US
                       </a>
 
